a24763977dbabc2953cfe27c217247bb2eca44e2
Jon Maldia <jon@maldia.net>
2018-08-01 23:38:25 -0400

    Refactor code to split functions

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,8 +1,36 @@
 console.log('Starting notes.js');
 
-let addNote = (title, body) => {
-    console.log(`Adding note: ${title} - ${body} `);
+const fs = require('fs');
+
+let fetchNotes = () => {
+    try {
+        let notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    } catch (e) {
+        return [];
+    }
 };
+
+let saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+let addNote = (title, body) => {
+    let notes = fetchNotes();
+    let note = {
+        title, 
+        body
+    };
+    let duplicateNotes = notes.filter((note) => note.title === title);
+    
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+    } else {
+        console.log('The note title already exists. Please change the title of your note.');
+    }
+};
+
 let getAll = () => {
     console.log(`Getting all notes`);
 };
