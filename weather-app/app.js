b57a89c3073eafdcbf98a7522b7016ef30aeafd7
Jon Maldia <jon@maldia.net>
2018-08-05 20:55:20 -0400

    Add code to grab data from body

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,10 +4,12 @@ request({
     url: 'http://maps.googleapis.com/maps/api/geocode/json?address=126%20hackett%20place%20rutherford',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(error, undefined, 2));
-    if (response.statusCode === 400) {
-        console.log(body.results[0].geometry.location.lat);
-        console.log(JSON.stringify(response, undefined, 2)); // How to pretty print on json on console
+    // console.log(JSON.stringify(error, undefined, 2));
+    if (response.statusCode === 200) {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+        // console.log(JSON.stringify(response, undefined, 2)); // How to pretty print on json on console
     }
 });
 
