e5aaeebe8940342e91978a192b841eadbdf48e2f
Jon Maldia <jon@maldia.net>
2018-08-07 18:10:11 -0400

    Refactor code to return an object instead of console logging things

--- weather-app/geocode/geocode.js
+++ weather-app/geocode/geocode.js
@@ -3,23 +3,23 @@ const request = require('request');
 // This function accepts one argument - the address
 // It takes the address and sends a request to the google maps api
 // The result sent 
-let geocodeAddress = (address) => {
+let geocodeAddress = (address, callback) => {
     let encodedAdress = encodeURIComponent(address);
 
     request({
         url: `http://maps.googleapis.com/maps/api/geocode/json?sensor=true&address=${encodedAdress}`,
         json: true
     }, (error, response, body) => {
-        // console.log(JSON.stringify(body, undefined, 2));
         if (error) {
-            console.log('Unable to connect with the Google servers');
+            callback('Unable to connect with the Google servers');
         } else if (body.status === 'ZERO_RESULTS') {
-            console.log('Unable to find the address');
+            callback('Unable to find the address');
         } else if (body.status === 'OK') {
-            console.log(`Address   : ${body.results[0].formatted_address}`);
-            console.log(`Latitude  : ${body.results[0].geometry.location.lat}`);
-            console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
-            // console.log(JSON.stringify(response, undefined, 2)); // How to pretty print on json on console
+            callback(undefined, {
+                address: body.results[0].formatted_address, 
+                latitude: body.results[0].geometry.location.lat, 
+                longitude: body.results[0].geometry.location.lng
+            });
         }
     });
 
